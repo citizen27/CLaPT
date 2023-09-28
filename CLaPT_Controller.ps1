dc31522b7d2dc29867593d4b4858876fc51f4ae0
Jacob Rodjenski <70776670+citizen27@users.noreply.github.com>
2023-09-28 19:44:25 -0400

    Adding invoker to Controller.ps1

--- CLaPT_Controller.ps1
+++ CLaPT_Controller.ps1
@@ -1,9 +1,14 @@
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\PantherParser\PantherELTParse.ps1"
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\PantherParser\PantherSetupActParser.ps1"
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\Security Event Log Parsing\EventLogTamper.ps1"
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\Security Event Log Parsing\FailedLogon.ps1"
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\ObjectAccess.ps1"
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\SystemTimeChange.ps1"
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\WinLogoff.ps1"
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\WinLogon.ps1"
-Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\ShutdownLog.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\PantherParser\PantherELTParse.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\PantherParser\PantherSetupActParser.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\Security Event Log Parsing\EventLogTamper.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\Security Event Log Parsing\FailedLogon.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\ObjectAccess.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\SystemTimeChange.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\WinLogoff.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\WinLogon.ps1"
+#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\ShutdownLog.ps1"
+
+
+$Master = Split-Path $MyInvocation.MyCommand.Definition -Leaf
+$ScriptPath = Split-Path $MyInvocation.MyCommand.Definition
+Get-ChildItem "$ScriptPath\*.ps1" | Where-Object{$_.Name -ne $Master} | ForEach-Object { & $_.FullName }
