b5cc75c21e81fc99602a5f92cda62211fab7bea9
Jacob Rodjenski <70776670+citizen27@users.noreply.github.com>
2023-09-28 20:20:11 -0400

    Edited Controller.ps1

--- CLaPT_Controller.ps1
+++ CLaPT_Controller.ps1
@@ -1,12 +1,12 @@
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\PantherParser\PantherELTParse.ps1"
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\PantherParser\PantherSetupActParser.ps1"
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\Security Event Log Parsing\EventLogTamper.ps1"
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\Security Event Log Parsing\FailedLogon.ps1"
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\ObjectAccess.ps1"
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\SystemTimeChange.ps1"
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\WinLogoff.ps1"
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\WinLogon.ps1"
-#Invoke-Command -FilePath "C:\Users\jacob\Desktop\CLaPT\ShutdownLog.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\PantherParser\PantherELTParse.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\PantherParser\PantherSetupActParser.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\Security Event Log Parsing\EventLogTamper.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\Security Event Log Parsing\FailedLogon.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\ObjectAccess.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\SystemTimeChange.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\WinLogoff.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\WinLogon.ps1"
+#Invoke-Command -FilePath "C:\Users\xxx\Desktop\CLaPT\ShutdownLog.ps1"
 
 
 $Master = Split-Path $MyInvocation.MyCommand.Definition -Leaf
