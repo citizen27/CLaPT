ccb2d2a434c50dc071236f6a7eb6bc5cc206c2c0
Jacob Rodjenski <70776670+citizen27@users.noreply.github.com>
2023-09-29 19:22:55 -0400

    Edits to controller script to handle errors

--- CLaPT_Controller.ps1
+++ CLaPT_Controller.ps1
@@ -1,6 +1,12 @@
 # Specify the root directory where you want to start the search
 $rootDirectory = "$home\Desktop\CLaPT\CLaPTRun\"
 
+# Specify the path for the error log file
+$errorLogFile = "$home\Desktop\CLaPT_Output\ErrorLog.txt"
+
+# Create an empty error log file or clear its contents if it already exists
+$null = New-Item -Path $errorLogFile -ItemType File -Force
+
 # Get a list of all script files recursively within the root directory
 $scriptFiles = Get-ChildItem -Path $rootDirectory -Filter *.ps1 -File -Recurse
 
@@ -8,9 +14,20 @@ $scriptFiles = Get-ChildItem -Path $rootDirectory -Filter *.ps1 -File -Recurse
 foreach ($scriptFile in $scriptFiles) {
     Write-Host "Executing script: $($scriptFile.FullName)"
     
-    # Execute the script
-    Invoke-Expression -Command (Get-Content -Path $scriptFile.FullName -Raw)
+    # Try to execute the script and capture any errors
+    try {
+        Invoke-Expression -Command (Get-Content -Path $scriptFile.FullName -Raw)
+    }
+    catch {
+        $errorMessage = "Error in $($scriptFile.FullName): $_"
+        Write-Host $errorMessage
+        # Append the error message to the error log file
+        Add-Content -Path $errorLogFile -Value $errorMessage
+    }
     
     # Add a separator for clarity
     Write-Host "------------------------------------------------------"
 }
+
+# Exit PowerShell
+Exit
