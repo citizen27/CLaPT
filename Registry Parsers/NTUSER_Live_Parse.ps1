0d2baab10fc51112cd8e79e595cd7b037b2c0e6a
Jacob Rodjenski <70776670+citizen27@users.noreply.github.com>
2023-10-08 20:34:40 -0400

    NTUSER_Live_Parse output formatting

--- Registry Parsers/NTUSER_Live_Parse.ps1
+++ Registry Parsers/NTUSER_Live_Parse.ps1
@@ -10,56 +10,59 @@ if (-not (Test-Path -Path $outputDirectory)) {
 
 # Define an array of registry keys you want to extract
 $registryKeys = @(
-    #Auto-Start
+    # Auto-Start
     "Software\Microsoft\Windows\CurrentVersion\Run",
     "Software\Microsoft\Windows\CurrentVersion\RunOnce",
     "Software\Microsoft\Windows\CurrentVersion\Runonce",
 
-    #Explorer Searches
+    # Explorer Searches
     "Software\Microsoft\Windows\CurrentVersion\Explorer\WordwheelQuery",
 
-    #Microsoft Office Server Connections
+    # Microsoft Office Server Connections
     "Software\Microsoft\Office\<Office Version>\Common\Open Find\Microsoft Office Server\Recent Server List",
 
-    #MRUs
+    # MRUs
     "Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedMRU",
     "Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU",
     "Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU",
     "Software\Microsoft\Windows\CurrentVersion\Explorer\Policies\RunMR",
     "Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU\",
 
-    #Mount Points2 (User Assigned Drive Letters)
+    # Mount Points2 (User Assigned Drive Letters)
     "Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2",
 
-    #Network Information
+    # Network Information
     "Software\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged & Software\Microsoft\Windows",
     "Software\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles",
     "Software\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Managed & Software\Microsoft\Windows NT\CurrentVersion\NetworkList\Nla\Cache",
 
-    #Printer Jobs and Connections 
+    # Printer Jobs and Connections 
     "\Printers\Connections",
 
-    #Recent Docs
+    # Recent Docs
     "Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs\",
     "Software\Microsoft\Office\*\*\FileMRU",
     "Software\Microsoft\Office\*\*\UserMRU\LiveID_###\FileMRU",
 
-    #Shellbags
+    # Shellbags
     "Software\Microsoft\Windows\Shell\BagMRU",
     "Software\Microsoft\Windows\Shell\Bags",
  
-    #Typed Paths (Win 10 only)
+    # Typed Paths (Win 10 only)
     "Software\Microsoft\Windows\CurrentVersion\Explorer\TypedPaths",
 
-    #Typed URLs
+    # Typed URLs
     "Software\Microsoft\Internet Explorer\TypedURLs",
 
-    #User Account Information
+    # User Account Information
     "Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs",
 
-    #User Assist 
+    # User Assist 
     "Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist\*\Count"
 )
+
+# Initialize an array to store skipped registry keys
+$skippedKeys = @()
 
 # Loop through the registry keys
 foreach ($keyPath in $registryKeys) {
@@ -108,9 +111,44 @@ foreach ($keyPath in $registryKeys) {
         $data | Export-Csv -Path $CSVFilePath -NoTypeInformation
 
         Write-Host "Data for key $keyPath has been exported to $CSVFilePath"
+
+        # Check if the CSV file exists before attempting to move it
+        if (Test-Path -Path $CSVFilePath) {
+            # Check if the CSV file is empty, and if so, move it to the "No Responsive Results" subfolder with a random number appended to the filename
+            if (!(Get-Content -Path $CSVFilePath)) {
+                $NoResponsiveResultsFolder = Join-Path -Path $outputDirectory -ChildPath "No Responsive Results"
+                if (-not (Test-Path -Path $NoResponsiveResultsFolder)) {
+                    New-Item -Path $NoResponsiveResultsFolder -ItemType Directory
+                }
+                $RandomNumber = Get-Random -Minimum 1 -Maximum 10000
+                $NewFileName = "$keyName-$RandomNumber.csv"
+                $NewFilePath = Join-Path -Path $NoResponsiveResultsFolder -ChildPath $NewFileName
+                Move-Item -Path $CSVFilePath -Destination $NewFilePath
+                Write-Host "No data for key $keyPath. CSV file moved to $NewFilePath"
+            }
+            else {
+                # Move the CSV file to the "Responsive Results" folder
+                $ResponsiveResultsFolder = Join-Path -Path $outputDirectory -ChildPath "Responsive Results"
+                if (-not (Test-Path -Path $ResponsiveResultsFolder)) {
+                    New-Item -Path $ResponsiveResultsFolder -ItemType Directory
+                }
+                $ResponsiveFilePath = Join-Path -Path $ResponsiveResultsFolder -ChildPath "$keyName.csv"
+                Move-Item -Path $CSVFilePath -Destination $ResponsiveFilePath
+                Write-Host "Data for key $keyPath has been moved to Responsive Results folder: $ResponsiveFilePath"
+            }
+        }
     }
     else {
-        # Write a message to the terminal if the registry key path doesn't exist and skip this key
+        # Add the skipped key to the array
+        $skippedKeys += $fullKeyPath
         Write-Host "Registry key path does not exist: $fullKeyPath. Skipping..."
     }
 }
+
+# Define the path for the skipped keys file
+$skippedKeysFilePath = Join-Path -Path $outputDirectory -ChildPath "NonExistingKeys.txt"
+
+# Write the skipped keys to the file
+$skippedKeys | Out-File -FilePath $skippedKeysFilePath
+
+Write-Host "Skipped keys have been saved to: $skippedKeysFilePath"
